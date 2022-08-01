fe86983f897a1b1eefbe0c3b8f99e782b99a4745
FatihG34 <fthgnydn34@gmail.com>
2022-08-01 16:21:23 +0300

    some code added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -61,7 +61,7 @@ const Navbar = () => {
                     </Box>
 
                     <Box>
-                        <Tooltip title="Open settings">
+                        <Tooltip title={currentUser ? "Open Personal Pages" : "Account Pages"}>
                             <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                                 {/* <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" /> */}
                                 <AccountCircleTwoTone />
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,7 +4,8 @@ import AlternateEmailSharpIcon from '@mui/icons-material/AlternateEmailSharp';
 import KeySharpIcon from '@mui/icons-material/KeySharp';
 import { Grid, Box, Stack, TextField, InputAdornment, FormControl, InputLabel, Select, MenuItem, Button } from '@mui/material'
 import blogimg from '../assets/blockimage.jpg';
-// import { Form } from 'formik';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
 
 const initialValue = { firstName: '', lastName: '', email: '', password: '' }
 
@@ -14,24 +15,43 @@ const Register = () => {
     const handleChange = (e) => {
         e.preventDefault();
         const { name, value } = e.target;
-        // setUserInfo({ ...userInfo, [name]: value })
+        // setUser({ ...user, [name]: value })
         // console.log(userInfo);
     }
     return (
         <div className='register'>
             <Grid style={{ width: '25rem', backgroundColor: 'whitesmoke', padding: '2rem', borderRadius: '0.75rem', boxShadow: '18px 18px 25px black' }} >
-                <div>
+                <div style={{ margin: '0 0 1rem 0' }}>
                     <img className='blogimg' src={blogimg} alt="blogimage" />
                     <h3>- Register -</h3>
                 </div>
-                <Box>
-                    <form>
+                <Formik
+                    initialValues={{ firstName: '', lastName: '', email: '', password: '' }}
+                    validationSchema={Yup.object().shape({
+                        firstName: Yup.string().max(25, 'You must enter a maximum of 25 characters').required('First Name information must be filled'),
+                        lastName: Yup.string().max(25, 'You must enter a maximum of 25 characters').required('Last Name information must be filled'),
+                        email: Yup.string().email('Please enter a valid e-mail address').required('e-mail information must be filled').matches(/([\w._%+-]+@[\w.-]+\.[a-zA-Z]{0,4})/, 'Such as : asdf@dfgv.com'),
+                        password: Yup.string().min(8).max(16).required('Password information must be filled').matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,16})/, "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character")
+                    })
+                    }
+                    onSubmit={(values, action) => {
+                        user.firstName = values.firstName;
+                        user.lastName = values.lastName;
+                        user.email = values.email;
+                        user.password = values.password;
+                        action.resetForm();
+                        action.setSubmitting(false)
+                    }}
+                >
+                    <Form>
                         <Stack spacing={4} direction='column' >
                             <TextField
-                                type='text'
+                                label='First Name'
                                 variant='outlined'
-                                value={null}
-                                name='username'
+                                id='firstName'
+                                type='text'
+                                name='firstName'
+                                value={user.firstName}
                                 onChange={handleChange}
                                 placeholder='First Name'
                                 InputProps={{
@@ -41,12 +61,14 @@ const Register = () => {
                                         </InputAdornment>
                                     ),
                                 }}
+
                             />
                             <TextField
+                                label='Last Name'
                                 type='text'
                                 variant='outlined'
-                                value={null}
-                                name='username'
+                                value={user.lastName}
+                                name='firstName'
                                 onChange={handleChange}
                                 placeholder='Last Name'
                                 InputProps={{
@@ -58,9 +80,10 @@ const Register = () => {
                                 }}
                             />
                             <TextField
+                                label='E-mail'
                                 type='email'
                                 variant='outlined'
-                                value={null}
+                                value={user.email}
                                 name='email'
                                 onChange={handleChange}
                                 placeholder='e-mail'
@@ -73,9 +96,10 @@ const Register = () => {
                                 }}
                             />
                             <TextField
+                                label='Password'
                                 type='password'
                                 variant='outlined'
-                                value={null}
+                                value={user.password}
                                 name='password'
                                 onChange={handleChange}
                                 placeholder='Password'
@@ -90,9 +114,8 @@ const Register = () => {
 
                             <Button variant='contained' type='submit' value='Submit' >Register</Button>
                         </Stack>
-                    </form>
-                </Box>
-
+                    </Form>
+                </Formik>
             </Grid>
         </div>
     )
