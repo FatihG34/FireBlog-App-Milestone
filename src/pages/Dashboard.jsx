0c61004edfe342ef338b21c3ce721e9f8c246d1b
FatihG34 <fthgnydn34@gmail.com>
2022-08-05 00:05:14 +0300

    dashboard created

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import { Box, Grid } from '@mui/material';
+
 
 const Dashboard = () => {
-    return (
-        <div>Dashboard</div>
-    )
-}
 
+    return (
+        <Box sx={{ display: 'grid', placeContent: 'center' }}>
+            <Grid container spacing={2} sx={{ margin: '1rem' }}>
+
+                <Grid item xs={8}>
+                    <Card sx={{ maxWidth: 500 }}>
+                        <CardMedia
+                            component="img"
+                            height="194"
+                            image="/static/images/cards/paella.jpg"
+                            alt="Paella dish"
+                        />
+                        <CardHeader
+                            title="Shrimp and Chorizo Paella"
+                            subheader="September 14, 2016"
+                        />
+                        <CardContent>
+                            <Typography variant="body2" color="text.secondary">
+                                This impressive paella is a perfect party dish and a fun meal to cook
+                                together with your guests. Add 1 cup of frozen peas along with the mussels,
+                                if you like.
+                            </Typography>
+                        </CardContent>
+                        <CardHeader
+                            avatar={
+                                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                                    R
+                                </Avatar>
+                            }
+                            title="Shrimp and Chorizo Paella"
+                        // title={data.owner}
+                        />
+                        <CardActions disableSpacing>
+                            <IconButton aria-label="add to favorites">
+                                <FavoriteIcon />
+                            </IconButton>
+                            <IconButton aria-label="share">
+                                <ChatBubbleOutlineOutlinedIcon />
+                            </IconButton>
+                        </CardActions>
+                    </Card>
+                </Grid>
+            </Grid>
+        </Box>
+    );
+}
 export default Dashboard
