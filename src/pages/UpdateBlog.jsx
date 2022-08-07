805729b387d557ab94136d21590134be2f63ab38
FatihG34 <fthgnydn34@gmail.com>
2022-08-07 22:35:41 +0300

    updateBlog page done

--- src/pages/UpdateBlog.jsx
+++ src/pages/UpdateBlog.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Button, Grid, Stack, TextField } from '@mui/material';
 import { Box } from '@mui/system';
 import blogimg from '../assets/blockimage.jpg';
@@ -16,26 +16,26 @@ const UpdateBlog = () => {
     const navigate = useNavigate()
     const { state } = useLocation();
     const { data } = state;
-    console.log('updateblog da ki data:', data)
+    const [updateData, setUpdateData] = useState(data)
 
     const handleChange = (e) => {
         e.preventDefault();
         const { name, value } = e.target;
         const blogUpdateTime = (new Date()).toString().slice(4, 16)
         // const bloger = { blogerId: currentUser.uid, blogerName: currentUser.displayName, blogerEmail: currentUser.email }
-        setBlogData({ ...data, [name]: value, blogUpdateTime })
+        setUpdateData({ ...updateData, [name]: value, blogUpdateTime })
     };
     const handleSubmit = (e) => {
         e.preventDefault();
-        UpdateData(data)
+        UpdateData(updateData)
         navigate('/')
     }
-
+    console.log(updateData);
     return (
         <Box sx={{ display: 'grid', placeContent: 'center' }}>
             <Grid style={{ width: '30rem', padding: '2rem' }}>
                 <Box style={{ textAlign: 'center', mb: 2 }}>
-                    <img className='blogimg' src={blogimg} alt="blogimage" />
+                    <img width={300} src={data.imageUrl} alt="blogimage" />
                     <h3>- Update Blog -</h3>
                 </Box>
                 <form
@@ -46,7 +46,7 @@ const UpdateBlog = () => {
                             label='Title *'
                             type='text'
                             name='title'
-                            value={data.title}
+                            value={updateData.title}
                             id="outlined-size-normal"
                             required
                             onChange={(e) => handleChange(e)}
@@ -55,7 +55,7 @@ const UpdateBlog = () => {
                             label='Image URL *'
                             type='url'
                             name='imageUrl'
-                            value={data.imageURL}
+                            value={updateData.imageURL}
                             id="outlined-size-normal"
                             onChange={handleChange}
                             // onChange={handleChange} same with above
@@ -64,7 +64,7 @@ const UpdateBlog = () => {
                         <TextField
                             label='Content *'
                             name='content'
-                            value={data.content}
+                            value={updateData.content}
                             multiline
                             rows={12}
                             maxRows={18}
